795717a8c40ddcad14e3e79fdbcfd49dee02e8f2
Scott Bradley <scottbradley.mail@gmail.com>
2017-03-01 12:22:02 -0600

    adds util; moves GPS into external library

--- sensor_grid.h
+++ sensor_grid.h
@@ -23,15 +23,9 @@ enum ERRORS {
      WIFI_MODULE_NOT_DETECTED
 };
 
+#include <KnightLab_GPS.h>
 #include "config.h"
-#include "modules/gps/GPS.h"
 #include "encryption.h"
-
-void fail(enum ERRORS err) {
-    Serial.print(F("ERR: "));
-    Serial.println(err);
-    while(1);
-}
 
 typedef struct Message {
     uint16_t ver_100;
@@ -73,40 +67,9 @@ If the address is $2000000 or larger, its in SRAM. If the address is between $00
 class __FlashStringHelper;
 #define F(string_literal) (reinterpret_cast<const __FlashStringHelper*>(PSTR(string_literal)))
 
-void flashLED(int times, int endState) {
-    digitalWrite(LED, LOW); delay(50);
-    for (int i=0; i<times; i++) {
-        digitalWrite(LED, HIGH); delay(50);
-        digitalWrite(LED, LOW); delay(50);
-    }
-    digitalWrite(LED, endState);
-}
-
-float batteryLevel() {
-    float measuredvbat = analogRead(VBATPIN);
-    measuredvbat *= 2;    // we divided by 2, so multiply back
-    measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-    measuredvbat /= 1024; // convert to voltage
-    return measuredvbat;
-}
-
-#if defined(__AVR_ATmega32U4__)
-int freeRam () {
-  extern int __heap_start, *__brkval; 
-  int v; 
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
-}
-#elif defined(ARDUINO_ARCH_SAMD)
-extern "C" char *sbrk(int i);
-int freeRam() {
-  char stack_dummy = 0;
-  return &stack_dummy - sbrk(0);
-}
-#endif
-
-void printRam() {
-    Serial.print(F("freeRam: "));
-    Serial.println(freeRam());
-}
+void fail(enum ERRORS err);
+void flashLED(int times, int endState);
+float batteryLevel();
+void printRam();
 
 #endif
---
+++ util.cpp
@@ -0,0 +1,43 @@
+#include "sensor_grid.h"
+
+#if defined(__AVR_ATmega32U4__)
+static int _freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+#elif defined(ARDUINO_ARCH_SAMD)
+extern "C" char *sbrk(int i);
+static int _freeRam() {
+  char stack_dummy = 0;
+  return &stack_dummy - sbrk(0);
+}
+#endif
+
+void fail(enum ERRORS err) {
+    Serial.print(F("ERR: "));
+    Serial.println(err);
+    while(1);
+}
+
+void flashLED(int times, int endState) {
+    digitalWrite(LED, LOW); delay(50);
+    for (int i=0; i<times; i++) {
+        digitalWrite(LED, HIGH); delay(50);
+        digitalWrite(LED, LOW); delay(50);
+    }
+    digitalWrite(LED, endState);
+}
+
+float batteryLevel() {
+    float measuredvbat = analogRead(VBATPIN);
+    measuredvbat *= 2;    // we divided by 2, so multiply back
+    measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+    measuredvbat /= 1024; // convert to voltage
+    return measuredvbat;
+}
+
+void printRam() {
+    Serial.print(F("Free RAM: "));
+    Serial.println(_freeRam());
+}
