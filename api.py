7f714a71c8c3f0e91ef8093c5de6c0820462b800
Scott Bradley <scottbradley.mail@gmail.com>
2017-09-05 13:00:58 -0500

    removes snd from api message handling

--- api.py
+++ api.py
@@ -39,23 +39,21 @@ def data():
     # http://werkzeug.pocoo.org/docs/0.11/wrappers/#werkzeug.wrappers.BaseRequest.get_data
     if request.json.get('ver') == '1': # json encoded message
         msg = request.json
-        ( ver, net, snd, orig, msg_id, bat, timestamp, data) = (
-            float(msg['ver']), int(msg['net']), int(msg['snd']),
+        ( ver, net, orig, msg_id, bat, timestamp, data) = (
+            int(msg['ver']), int(msg['net']),
             int(msg['orig']), int(msg['id']),
             float(msg['bat']), int(msg['timestamp']),
             [int(d) for d in msg['data']])
     else: # raw struct message
         msg = request.get_data(cache=False)
-        ver_100, net, snd, orig, msg_id, bat_100, timestamp, \
+        ver, net, orig, msg_id, bat_100, timestamp, \
             *data = unpack(PROTOCOL, msg)
-        ver = ver_100 / 100.0
         bat = bat_100 / 100.0
     sys.stdout.flush()
     global history
     record = {
         'version': ver,
         'network': net,
-        'snd': snd,
         'message_id': msg_id,
         'received_at': datetime.datetime.now(),
         'node_id': orig,
