c5ecab960674f2997a042dfdb4091e61e53909ab
vcabales <vcabales96@gmail.com>
2017-10-30 10:59:35 -0500

    WiFi101 WiFi connect added

--- WINC1500.h
+++ WINC1500.h
@@ -1,5 +1,6 @@
 #ifndef WINC1500_H
 #define WINC1500_H
+#include <WiFi101.h>
 
 
 #if defined(__AVR_ATmega32U4__)
@@ -18,5 +19,6 @@ void printWifiStatus();
 // bool postToAPI(const char* wifi_ssid, const char* wifi_pass, const char* apiServer, const char* apiHost, const int apiPort, uint8_t msgBytes[], uint8_t msgLen);
 bool postToAPI(const char* wifi_ssid, const char* wifi_pass, const char* apiServer, const char* apiHost, const int apiPort, char* msg, uint8_t msgLen);
 bool setupWiFi(const char* wifi_ssid, const char* wifi_pass);
+void connectToServer(WiFiClient& client,char ssid[],char pass[]);
 
 #endif
