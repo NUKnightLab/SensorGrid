89a7f0bbb994fcd37fb1a83c6a03921b6399c17a
Scott Bradley <scottbradley.mail@gmail.com>
2018-01-25 14:59:04 -0600

    node id 1

--- RadioTest/RadioTest.ino
+++ RadioTest/RadioTest.ino
@@ -4,7 +4,7 @@
 #include <SPI.h>
 
 /* SET THIS FOR EACH NODE */
-#define NODE_ID 3 // 1 is collector; 2,3 are sensors
+#define NODE_ID 1 // 1 is collector; 2,3 are sensors
 
 #define FREQ 915.00
 #define TX 5
