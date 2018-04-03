d11b0333b2f86493c0206895ef6c4a012ce120fe
Scott Bradley <scottbradley.mail@gmail.com>
2018-04-03 15:28:46 -0500

    eeprom random test and byte test

--- EEPROM/EEPROM.ino
+++ EEPROM/EEPROM.ino
@@ -12,6 +12,9 @@
 
 //#define MAX_EEPROM_ADDR 0x7FFF
 #define MAX_EEPROM_ADDR 100
+bool TEST_ODD_BITS = false;
+bool TEST_EVEN_BITS = false;
+bool TEST_RANDOM_CHECKSUMS = false;
 
 bool core = true;
 
@@ -24,8 +27,11 @@ void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data
     Wire.endTransmission();
 }
 
-// WARNING: address is a page address, 6-bit end will wrap around
-// also, data can be maximum of about 30 bytes, because the Wire library has a buffer of 32 bytes
+/**
+ *  WARNING: address is a page address, 6-bit end will wrap around
+ *  also, data can be maximum of 30 bytes, because the Wire library has a buffer of 32 bytes
+ *  (2 are used for the write address)
+ */
 void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte* data, byte len ) {
     if (len > 30) len = 30;
     byte buf[30] = {};
@@ -39,7 +45,31 @@ void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte*
     delay(100);
 }
 
-void i2c_eeprom_read_page(byte* buf, uint8_t* buflen, int deviceaddress, unsigned int eeaddress) {
+/**
+ * 2 bytes are used for the checksum. Max data length is 28 bytes and will be truncated
+ */
+void i2c_eeprom_write_checked_page(int deviceaddress, unsigned int eeaddresspage, byte* data, byte len)
+{
+    if (len > 28) len = 28;
+    byte buf[30] = {};
+    int sum = 0;
+    for (int i=0; i<len; i++) {
+        sum += data[i];
+    }
+    buf[0] = sum>>8;
+    buf[1] = sum&0xFF;
+    memcpy(buf+2, data, len);
+    Wire.beginTransmission(deviceaddress);
+    Serial.print("WRITING Addr: "); Serial.println(eeaddresspage, HEX);
+    Wire.write((int)(eeaddresspage >> 8)); // MSB
+    Wire.write((int)(eeaddresspage & 0xFF)); // LSB
+    Wire.write(buf, 30);
+    Wire.endTransmission();
+    delay(100);
+}
+
+
+void i2c_eeprom_read_page(byte* buf, int8_t* buflen, int deviceaddress, unsigned int eeaddress) {
     Wire.beginTransmission(deviceaddress);
     Wire.write((int)(eeaddress >> 8)); // MSB
     Wire.write((int)(eeaddress & 0xFF)); // LSB
@@ -50,6 +80,35 @@ void i2c_eeprom_read_page(byte* buf, uint8_t* buflen, int deviceaddress, unsigne
         buf[i++] = Wire.read();
     }
     *buflen = i;
+}
+
+void i2c_eeprom_read_checked_page(byte* buf, int8_t* buflen, int deviceaddress, unsigned int eeaddress) {
+    Wire.beginTransmission(deviceaddress);
+    Wire.write((int)(eeaddress >> 8)); // MSB
+    Wire.write((int)(eeaddress & 0xFF)); // LSB
+    Wire.endTransmission();
+    Wire.requestFrom(deviceaddress, *buflen);
+    int checksum = Wire.read()<<8 | Wire.read()&0xFF;
+    int sum = 0;
+    uint8_t i = 0;
+    while (i< *buflen && Wire.available()) {
+        byte b = Wire.read();
+        buf[i++] = b;
+        sum += b;
+    }
+    if (sum == checksum) {
+        Serial.print("Addr: ");
+        Serial.print(eeaddress>>8 | eeaddress&0xFF, DEC);
+        Serial.print(" OK. sum: ");
+        Serial.print(checksum, DEC);
+        *buflen = i;
+    } else {
+        Serial.print("** WARNING: addr: ");
+        Serial.print(eeaddress, DEC);
+        Serial.print(" Sum: "); Serial.print(sum, DEC);
+        Serial.print(" != "); Serial.print(checksum, DEC);
+        *buflen = -1;
+    }
 }
 
 byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
@@ -77,7 +136,7 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
 
 void clear_data()
 {
-    /** 
+    /**
      *  Wire library buffer is 32 bytes. There is 2-bytes addressing overhead. EEPROM page size is 64 bytes so we
      *  clear a page in 3 separate writes. Ideally we would have a Wire buffer of >= 66 bytes in order to clear a whole
      *  page at once.
@@ -91,84 +150,128 @@ void clear_data()
         i2c_eeprom_write_page(0x50, addr+44, nodata_20, 20);
         addr += 64;
     }
-    delay(500);
+    delay(500); // Max page write time
+}
+
+/**
+ * Check the write of a standard value to all bytes of all pages
+ */
+void test_bytes(byte val)
+{
+    int pagecount = 0;
+    for (int pageaddr=0; pageaddr<=MAX_EEPROM_ADDR; pageaddr+=32) {
+        byte testdata[30] = {};
+        int index = 0;
+        for (int i=0; i<30; i++) {
+            testdata[index++] = val;
+        }
+        i2c_eeprom_write_page(0x50, pageaddr, testdata, 30);
+        pagecount++;
+    }
+    int bad_page_count = 0;
+    int bad_byte_count = 0;
+    Serial.print("Value (");
+    Serial.print(val, DEC);
+    Serial.print(") test pages written: "); Serial.println(pagecount);
+    delay(2000);
+    Serial.println("\nReading pages:");
+    for (int i=0; i<=MAX_EEPROM_ADDR; i+=32) {
+        int bad_bytes = 0;
+        byte buf[30];
+        int8_t buflen = 30;
+        i2c_eeprom_read_page(buf, &buflen, 0x50, i);
+        Serial.print(" Data: ");
+        for (int j=0; j<buflen; j++) {
+            Serial.print(buf[j], DEC);
+            Serial.print(" ");
+            if (buf[j] != val) {
+                bad_bytes++;
+            }
+        }
+        Serial.println("");
+        if (bad_bytes) {
+            bad_byte_count += bad_bytes;
+            bad_page_count++;
+        }
+    }
+    Serial.print(bad_byte_count, DEC);
+    Serial.println(" bad bytes");
+    Serial.print(bad_page_count, DEC);
+    Serial.println(" bad pages");
+}
+
+void test_random_writes()
+{
+    int pagecount = 0;
+    for (int pageaddr=0; pageaddr<=MAX_EEPROM_ADDR; pageaddr+=32) {
+        byte testdata[28] = {};
+        int r = (rand() % 28) + 1;
+        int index = 0;
+        int sum = 0;
+        for (int i=0; i<r; i++) {
+            byte b = rand() % 0xFF;
+            sum += b;
+            testdata[index++] = b;
+        }
+        i2c_eeprom_write_checked_page(0x50, pageaddr, testdata, 28);
+        pagecount++;
+    }
+    int bad_page_count = 0;
+    Serial.print("Random test pages written: "); Serial.println(pagecount);
+    delay(2000);
+    Serial.println("\nReading pages:");
+    for (int i=0; i<=MAX_EEPROM_ADDR; i+=32) {
+        byte buf[30];
+        int8_t buflen = 30;
+        i2c_eeprom_read_checked_page(buf, &buflen, 0x50, i);
+        if (buflen < 0) {
+            Serial.println("CORRUPTED DATA");
+            bad_page_count++;
+        } else {
+            Serial.print(" Data: ");
+            for (int j=0; j<buflen; j++) {
+                Serial.print(buf[j], DEC);
+                Serial.print(" ");
+            }
+            Serial.println("");
+        }
+    }
+    Serial.print(bad_page_count, DEC);
+    Serial.println(" bad pages");
+}
+
+void read_all_data()
+{
+    for (int pageaddr=0; pageaddr<=MAX_EEPROM_ADDR; pageaddr+=32) {
+        byte buf[30];
+        int8_t buflen = 30;
+        i2c_eeprom_read_checked_page(buf, &buflen, 0x50, pageaddr);
+        for (int j=0; j<buflen; j++) {
+            Serial.print(buf[j], DEC);
+            Serial.print(" ");
+        }
+        Serial.println("");
+    }
 }
 
 void setup()
 {
-   srand(time(NULL));
-   Wire.begin();
-   while (!Serial);
-   Serial.begin(9600);
-   if (core) {
-      //byte testdata[30] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-      //byte testdata = {};
-      Serial.println("Clearing data ..");
-      //clear_data();
-      Serial.println(".. cleared");
-      Serial.println("Writing test 30-byte half-pages");
-      int pagecount = 0;
+    randomSeed(analogRead(0));
+    Wire.begin();
+    while (!Serial);
+    Serial.begin(9600);
+    if (TEST_ODD_BITS)
+        test_bytes(0b10101010);
+    if (TEST_EVEN_BITS)
+        test_bytes(0b01010101);
+    if (TEST_RANDOM_CHECKSUMS)
+        test_random_writes();
 
-/*
-      for (int pageaddr=0; pageaddr<=0x7FFF; pageaddr+=32) {  
-          testdata[0] = pageaddr >> 8;
-          testdata[1] = pageaddr & 0xFF;
-          i2c_eeprom_write_page(0x50, pageaddr, testdata, 30); // Arduino wire buffer is 32 bytes. 2 bytes EEPROM addr overhead, thus 30
-          pagecount++;
-          // TODO: can we do ACKnowledged polling? See datasheet p. 11
-          delay(100); // max page write time == 500ms
-      }
-*/
-      for (int pageaddr=0; pageaddr<=MAX_EEPROM_ADDR; pageaddr+=32) {
-          byte testdata[30] = {};
-          int r = (rand() % 29) + 1;
-          int index = 1;
-          //testdata[index++] = r;
-          int sum = 0;
-          for (int i=0; i<r; i++) {
-              byte b = rand() % 0xFF;
-              sum += b;
-              testdata[index++] = b;
-          }
-          testdata[0] = sum;
-          //testdata[index] = sum;
-          i2c_eeprom_write_page(0x50, pageaddr, testdata, 30);
-          pagecount++;
-      }
-      Serial.print("Test pages written: "); Serial.println(pagecount);
-      delay(2000);
-      Serial.println("\nReading pages:");
-      for (int i=0; i<=MAX_EEPROM_ADDR; i+=32) {
-          Serial.print("Addr: "); Serial.print(i>>8, DEC); 
-          Serial.print(" "); Serial.print(i&0xFF);
-          Serial.print(" Data: ");
-          byte buf[30];
-          uint8_t buflen = 30;
-          i2c_eeprom_read_page(buf, &buflen, 0x50, i);
-          int sum = 0;
-          for (int j=1; j<buflen; j++) {
-              sum += buf[j];
-              Serial.print(buf[j], DEC);
-              Serial.print(" ");
-          }
-          Serial.println("");
-          if (sum != buf[0]) {
-              Serial.print("WARNING: addr: ");
-              Serial.print(i, DEC);
-              Serial.print(" Sum: "); Serial.print(sum);
-              Serial.print(" != "); Serial.println(buf[0]);
-          }
-          Serial.println("OK");
-      }
-   }
-
-    //char somedata[] = "this is data from the eeprom"; // data to write
-    //i2c_eeprom_write_page(0x50, 0x7FFF, (byte *)somedata, sizeof(somedata)); // write to EEPROM
-    //delay(100);
-    //i2c_eeprom_write_page(0x50, 1, (byte *)somedata, sizeof(somedata)); // write to EEPROM
-    //delay(100); //add a small delay
-    //Serial.println("Memory written");
-
+    randomSeed(100);
+    if (core) {
+        test_random_writes();
+        read_all_data();
+    }
 }
 
 void loop()
@@ -199,7 +302,7 @@ void loop()
             addr++;
             if (b!=0) {
                 Serial.println("");
-            } 
+            }
         }
     }
 }
