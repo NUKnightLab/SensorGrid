7629ef8e1ce4731a2b3f90b214e6dff7ee9bc562
knightlab <vcabales96@gmail.com>
2018-05-07 09:23:54 -0500

    char data in Message struct

--- KnightLab_SensorGrid/SensorGridLoRaCollector/SensorGridLoRaCollector.ino
+++ KnightLab_SensorGrid/SensorGridLoRaCollector/SensorGridLoRaCollector.ino
@@ -3,19 +3,22 @@
 
 void setup()
 {
-    Serial.begin(9600);
-    //delay(10000);
+    //Serial.begin(9600);
+    delay(10000);
     loadConfig();
     Serial.println("Config loaded");
     //delay(1000);
-    //setup_radio(config.RFM95_CS, config.RFM95_INT, config.node_id);
+    setup_radio(config.RFM95_CS, config.RFM95_INT, config.node_id);
 }
 
 void loop()
 {
     static int i = 0;
     Serial.println(i++, DEC);
-    static Message msg[RH_ROUTER_MAX_MESSAGE_LEN] = {0};
+    static uint8_t buf[140] = {0};
+    //static Message msg[sizeof(Message)] = {0};
+    static Message *msg = (Message*)buf;
+    
     /*
     Message msg = {
         .sensorgrid_version=config.sensorgrid_version,
@@ -26,15 +29,18 @@ void loop()
     };
     */
     //uint8_t *_msg = (uint8_t*)&msg;
-    uint8_t len = sizeof(msg);
-    uint8_t toID = 2;
+    //uint8_t len = sizeof(msg);
+    //uint8_t toID = 2;
     //send_message((uint8_t*)&msg, len, toID);
+
     if (RECV_STATUS_SUCCESS == receive(msg, 60000)) {
         Serial.println("Received message");
         Serial.print("VERSION: "); Serial.println(msg->sensorgrid_version, DEC);
-        Serial.print("DATA: "); logln((char*)msg->data);
+        Serial.println("DATA: ");
+        logln((char*)(msg->data));
     } else {
         Serial.println("No message received ");
     }
+
     //delay(10000);
 }
