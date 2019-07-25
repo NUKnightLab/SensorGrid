f108f2e532d0d0d359cda5468a51be55f5ced9c8
Scott Bradley <scottbradley.mail@gmail.com>
2019-07-25 16:09:57 -0500

    collector running with modified LoRa library but freezes on tx

--- SensorGridLoRaCollector/SensorGridLoRaCollector.ino
+++ SensorGridLoRaCollector/SensorGridLoRaCollector.ino
@@ -1,11 +1,48 @@
+#include <LoRa.h>
 #include "config.h"
-#include "lora.h"
+//#include "lora.h"
 #include <WiFi101.h>
+#include <LoRaHarvest.h>
+#include <console.h>
+#include <DataManager.h>
 
 #define WIFI_CS 8
 #define WIFI_IRQ 7
 #define WIFI_RST 4
 #define WIFI_EN 2
+
+#define NODE_ID 1
+
+
+uint8_t nodes[2] = { 2, 3 };
+uint8_t routes[255][6] = {
+    { 0 },
+    { 0 },
+    { 1, 2 },
+    { 1, 2, 3 },
+    { 1, 2, 3, 6, 5, 4 },
+    { 1, 3, 6, 5 },
+    { 1, 2, 3, 6 }
+};
+
+static unsigned long previousMillis = 0;
+static unsigned long interval = 60000;
+
+bool runEvery()
+{
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        return true;
+    }
+    return false;
+}
+
+unsigned long nextCollection()
+{
+    return previousMillis + interval;
+}
+
 
 WiFiClient client;
 static bool wifi_present = false;
@@ -100,6 +137,17 @@ void postToAPI(WiFiClient& client, Message* msg)
     // client.println("Connection: close"); //close connection before sending a new request
 }
 
+bool scheduleDataSample(unsigned long interval)
+{
+    static unsigned long previousMillis = 0;
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        return true;
+    }
+    return false;
+}
+
 void setup()
 {
     // Serial.begin(9600);
@@ -107,7 +155,7 @@ void setup()
     loadConfig();
     Serial.println("Config loaded");
     // delay(1000);
-    setup_radio(config.RFM95_CS, config.RFM95_INT, config.node_id);
+    //setup_radio(config.RFM95_CS, config.RFM95_INT, config.node_id);
     if (config.wifi_password) {
         Serial.print("Attempting to connect to Wifi: ");
         Serial.print(config.wifi_ssid);
@@ -119,15 +167,29 @@ void setup()
     } else {
         Serial.println("No WiFi configuration found");
     }
+    nodeId(NODE_ID);
+    if (nodeId() == 1) isCollector = true;
+    println("Configuring LoRa with pins: CS: %d; RST: %d; IRQ: %d",
+        config.RFM95_CS, config.RFM95_RST, config.RFM95_INT);
+    LoRa.setPins(config.RFM95_CS, config.RFM95_RST, config.RFM95_INT);
+    if (!LoRa.begin(915E6)) {
+        //print("version: %x", LoRa.readRegister(0x42));
+        Serial.println("LoRa init failed");
+        while(true);
+    }
+    LoRa.enableCrc();
+    LoRa.onReceive(onReceive);
+    LoRa.receive();
+    rtcz.begin();
 }
 
-void loop()
+void _loop()
 {
     static int i = 0;
     Serial.println(i++, DEC);
-    static uint8_t buf[RH_ROUTER_MAX_MESSAGE_LEN];
+    //static uint8_t buf[RH_ROUTER_MAX_MESSAGE_LEN];
     //static Message msg[sizeof(Message)] = {0};
-    static Message *msg = (Message*)buf;
+    //static Message *msg = (Message*)buf;
  
     /*
     Message msg = {
@@ -143,6 +205,7 @@ void loop()
     // uint8_t toID = 2;
     // send_message((uint8_t*)&msg, len, toID);
 
+/*
     if (RECV_STATUS_SUCCESS == receive(msg, 60000)) {
         Serial.println("Received message");
         Serial.print("VERSION: "); Serial.println(msg->sensorgrid_version, DEC);
@@ -169,6 +232,116 @@ void loop()
     } else {
         Serial.println("No message received ");
     }
+  */
 
     // delay(10000);
+}
+
+void loop() {
+    static unsigned long tick_time = 0;
+    if (millis() > tick_time) {
+        print(".");
+        tick_time = millis() + 1000;
+    }
+
+    static unsigned long timeout = 0;
+    static uint8_t seq = 0;
+    if (isCollector && runEvery()) collectingData(true);
+    if (!isCollector && scheduleDataSample(5000)) recordBattery();
+    if (collectingData()) {
+        if (waitingPacket()) {
+            if (millis() > timeout) {
+                println("TIMEOUT");
+                collectingNodeIndex(-1);
+                collectingPacketId(1);
+                collectingData(false); // TODO: retry data fetch
+                waitingPacket(false);
+            }
+        } else {
+            collectingPacketId(collectingPacketId() - 1);
+            if (collectingPacketId() == 0) {
+                // send collected node data to the API
+                println("***** SEND DATA TO API *****");
+                print("{ node: %d, data: [", nodes[collectingNodeIndex()]);
+                for (int i=0; i<numBatches(0); i++) {
+                    char *batch = (char*)getBatch(i);
+                    print(batch);
+                    //for (int j=0; j<10; j++) print("%c", batch[j]);
+                    if (i < numBatches(0)-1) print(",");
+                }
+                println("]}");
+                println("**********");
+                clearData();
+                collectingNodeIndex(collectingNodeIndex() + 1);
+            }
+            if (collectingNodeIndex() >= sizeof(nodes)) {
+                for (int i=0; i<sizeof(nodes); i++) {
+                    // send shutdown
+                    LoRa.idle();
+                    LoRa.beginPacket();
+                    LoRa.write(routes[nodes[i]][1]);
+                    LoRa.write(nodeId());
+                    LoRa.write(nodes[i]);
+                    LoRa.write(++++seq);
+                    LoRa.write(PACKET_TYPE_STANDBY);
+                    writeTimestamp();
+                    LoRa.write(routes[nodes[i]], sizeof(routes[nodes[i]]));
+                    LoRa.write(0); // end route
+                    LoRa.write(nextCollection() / 1000);
+                    LoRa.endPacket();
+                    LoRa.receive();
+                }
+                collectingNodeIndex(-1);
+                collectingPacketId(1);
+                collectingData(false);
+                waitingPacket(false);
+                // TODO: send standby
+                return;
+            }
+            println("prefetch collection state: collecting: %d, waiting: %d, node idx: %d, packet: %d",
+                collectingData(), waitingPacket(), collectingNodeIndex(), collectingPacketId());
+            waitingPacket(true);
+            uint8_t node_id = nodes[collectingNodeIndex()];
+            uint8_t *route = routes[node_id];
+            uint8_t route_size = 0;
+            while (route_size < sizeof(route) && route[route_size] > 0) route_size++;
+            Serial.print("Fetching data from: ");
+            Serial.print(node_id);
+            Serial.print("; ROUTE: ");
+            for (int j=0; j<route_size; j++) {
+                Serial.print(route[j]);
+                Serial.print(" ");
+            }
+            Serial.println("");
+            LoRa.idle();
+            LoRa.beginPacket();
+            LoRa.write(route[1]);
+            LoRa.write(nodeId());
+            LoRa.write(node_id);
+            LoRa.write(++++seq);
+            LoRa.write(PACKET_TYPE_SENDDATA);
+
+            writeTimestamp();
+
+            LoRa.write(route, route_size);
+            LoRa.write(0); // end route
+            LoRa.write(collectingPacketId()); // packet id
+            LoRa.endPacket();
+            timeout = millis() + 10000;
+            println("set timeout to: %d", timeout);
+            LoRa.receive();
+            //Serial.println("Sending broadcast standby");
+            //LoRa.idle();
+            //LoRa.beginPacket();
+            //LoRa.write(255);
+            //LoRa.write(NODE_ID);
+            //LoRa.write(255);
+            //LoRa.write(++++seq);
+            //LoRa.write(PACKET_TYPE_STANDBY);
+            //LoRa.write(0);
+            //LoRa.write(20); // 20 seconds
+            //LoRa.endPacket();
+            //LoRa.receive();
+        }
+    }
 }
